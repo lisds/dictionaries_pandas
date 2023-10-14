d36a02ca299dc2a347b90896184c187a83f0ce9b
Matthew Brett <matthew.brett@gmail.com>
2023-10-14 21:49:23 +0100

    Update from template

--- tests/q3.py
+++ tests/q3.py
@@ -19,7 +19,7 @@ test = {
                 {
           'code': r"""
           >>> # Your function is not retrieving the correct rows...
-          >>> all((get_year_for_country(2009, 'AFG').values == for_marking.loc[9].values)[0])
+          >>> (np.array(get_year_for_country(2009, 'AFG')) == np.array(for_marking.loc[9])).all()
           True
 
           """,
